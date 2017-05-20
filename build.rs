d9021a700a950efa8b0e4ce06fab53cc3e3d0eec
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2017-05-20 10:30:21 +0100

    Update to fix previous break, and added mac release binary to the build folder

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
+use std::path::Path;
+use std::env;
+
 fn main() {
-     fn main() {
-          let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-          println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("build").display());
-     }
+     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+     println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("build").display());
 }
+
