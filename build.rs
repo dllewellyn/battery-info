81a273911603c2fd38b9e70cf00dafca1c0746bd
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2017-05-20 10:25:02 +0100

    Update to build.rs to relative paths

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
-     println!(r"cargo:rustc-link-search=build");
+     fn main() {
+          let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+          println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("build").display());
+     }
 }
